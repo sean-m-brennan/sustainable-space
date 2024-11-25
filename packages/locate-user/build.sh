bb43584b9a7cb2db655ddf1032aea937b32c80cb
Sean M. Brennan <sean_m_brennan@mail.com>
2024-11-25 14:31:43 -0600

    Remove directories that are now repositories

--- packages/locate-user/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Generate typescript code
-node src/tz/ingest_tz_table.node.js
--- packages/space-sim/src/images/credits.ts
+++
@@ -1,5 +0,0 @@
-export const credits = [
-  {"url": "https://svs.gsfc.nasa.gov", "label": "NASA Scientific Visualization Studio"},
-  {"url": "https://eoimages.gsfc.nasa.gov", "label": "NASA Earth Observatory"},
-  {"url": "https://www.freepik.com/free-vector/planets-galaxy_4228290.htm", "label": "Icons designed by Freepik"}
-]
